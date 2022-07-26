e7bbed097bc554e7cf83837e18fe6b2608c07501
Laboratório Didático <Laboratrio Didtico@150.162.132.145>
2022-07-26 15:22:00 -0300

    Adição tela cadastro interessado.

--- ConstrucaoInterfacesGraficas/src/aplicacao/view/TelaCadastroInteressado.java
+++ ConstrucaoInterfacesGraficas/src/aplicacao/view/TelaCadastroInteressado.java
@@ -1,42 +1,77 @@
 package aplicacao.view;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class TelaCadastroInteressado extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaCadastroInteressado frame = new TelaCadastroInteressado();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
+	
+	private FlowLayout layout;
+	private JLabel lblTitulo;
+	private JLabel lblNome;
+	private JLabel lblEndereco;
+	private JLabel lblNumero;
+	private JLabel lblComplemento;
+	private JLabel lblEmail;
+	private JLabel lblTelefone;
+	private JTextField txtNome;
+	private JTextField txtEndereco;
+	private JTextField txtNumero;
+	private JTextField txtComplemento;
+	private JTextField txtEmail;
+	private JTextField txtTelefone;
+	private JButton btnCadastrar;
+	private JButton btnLimpar;
+	private JButton btnVoltar;
+	
+	
 	public TelaCadastroInteressado() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		
+		criar();
+		configurar();
+		adicionar();
+		
+	}
+	
+	private void criar() {
+		
+		this.layout = new FlowLayout();
+		lblNome = new JLabel("Nome: ");
+		lblEndereco = new JLabel("Endereço: ");
+		lblNumero = new JLabel("Número: ");
+		lblComplemento = new JLabel("Complemento: ");
+		lblEmail = new JLabel("E-mail: ");
+		lblTelefone = new JLabel("Telefone: ");
+		txtNome = new JTextField(20);
+		txtEndereco = new JTextField(50);
+		txtNumero = new JTextField(10);
+		txtComplemento = new JTextField(20);
+		txtEmail = new JTextField(20);
+		txtTelefone = new JTextField(10);
+
+		
+	}
+	
+	private void configurar() {
+		
+		this.setLayout(layout);
+		this.setSize(800, 700);
+		this.setTitle("Cadastro de Interessado(a)");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	private void adicionar() {
+		
+		add(lblNome);
+		add(txtNome);
+		add(lblEndereco);
+		add(txtEndereco);
+		add(lblNumero);
+		add(txtNumero);
+		
 	}
 
 }
